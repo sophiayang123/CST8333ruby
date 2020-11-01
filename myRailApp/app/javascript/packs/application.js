c5bc5eec8fc8c85a2821b0257b663a3ba2f08f57
shuting yang <sophia12332111@gmail.com>
2020-11-01 18:21:04 -0500

    a4 complete

--- myRailApp/app/javascript/packs/application.js
+++ myRailApp/app/javascript/packs/application.js
@@ -8,6 +8,22 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+window.jQuery = window.$ = require('jquery');
+
+// Add DataTables jQuery plugin
+// require('imports-loader?define=>false!datatables.net')(window, $)
+// require('imports-loader?define=>false!datatables.net-select')(window, $)
+
+// Load datatables styles
+import 'datatables.net';
+import 'datatables.net-dt/css/jquery.dataTables.css'
+// import 'datatables.net-select-dt/css/select.dataTables.css'
+
+$(document).ready(function () { 
+    console.log("123")
+    $("#datatable").dataTable()
+ })
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
